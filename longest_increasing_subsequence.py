82877f66f0e23cb9a95a3a7a06a7d35d56d392d0
mudliew <liaojpeamil@163.com>
2016-06-06 16:36:44 +0800

    longest increasing sub-sequence

---
+++ longest_increasing_subsequence.py
@@ -0,0 +1,41 @@
+from graph import *
+from collections import namedtuple
+
+
+def longest_increasing_subsequence(seq):
+    def get_max_idx(seqnode_list):
+        if not seqnode_list:
+            return -1
+        return max(seqnode_list, key=lambda x: l[x.idx]).idx
+
+    SeqNode = namedtuple('SeqNode', ['idx', 'value'])
+    edge_reverse = {SeqNode(idx, node): [] for idx, node in enumerate(seq)}
+    print edge_reverse
+    length = len(seq)
+    l = [1] * length
+    prev = {idx: -1 for idx in xrange(length)}
+    for i in xrange(length):
+        for j in xrange(i + 1, len(seq)):
+            if seq[i] < seq[j]:
+                edge_reverse[SeqNode(j, seq[j])].append(SeqNode(i, seq[i]))
+    print edge_reverse
+    for idx in xrange(length):
+        prev_idx = get_max_idx(edge_reverse[SeqNode(idx, seq[idx])])
+        if prev_idx != -1:
+            l[idx] = 1 + l[prev_idx]
+            prev[idx] = prev_idx
+    return max(enumerate(l), key=lambda x: x[1]), prev
+
+
+def rebulid_path(prev, dest_node):
+    path = [dest_node]
+    while prev[dest_node] != -1:
+        path.append(prev[dest_node])
+        dest_node = prev[dest_node]
+    path.reverse()
+    return path
+
+
+seq = [5, 2, 8, 6, 3, 6, 9, 7]
+(idx, max_len), prev = longest_increasing_subsequence(seq)
+print rebulid_path(prev, idx)
